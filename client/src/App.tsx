2c8bdc0e8ad35334ecbab9248140facf22b3fa0a
BYB Digital <buildyourbranddigital@gmail.com>
2025-09-12 14:04:17 +0000

    as

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,22 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import { Router as WouterRouter, Switch, Route } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
+
 import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
+
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
+import ScrollToTop from "@/components/ScrollToTop";
+
 import Home from "@/pages/home";
 import HowItWorks from "@/pages/how-it-works";
 import Gallery from "@/pages/gallery";
 import Pricing from "@/pages/pricing";
 import FAQ from "@/pages/faq";
 import Contact from "@/pages/contact";
-import ScrollToTop from "@/components/ScrollToTop";
+import NotFound from "@/pages/not-found";
 
-function Router() {
+function AppRoutes() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -26,16 +30,17 @@ function Router() {
   );
 }
 
-function App() {
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <ScrollToTop /> {/* Add this line */}
-        <Router />
+        <ScrollToTop />
+        {/* Hash-based routing for GitHub Pages */}
+        <WouterRouter hook={useHashLocation}>
+          <AppRoutes />
+        </WouterRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
 }
-
-export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
 import { HashRouter } from "react-router-dom";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
