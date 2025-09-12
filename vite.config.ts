60d0d9d767a43b922b04f2a58144d97a71d67650
BYB Digital <buildyourbranddigital@gmail.com>
2025-09-12 13:37:20 +0000

    changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "./", // 👈 this makes asset paths relative (./assets/...)
   plugins: [
     react(),
     runtimeErrorOverlay(),
