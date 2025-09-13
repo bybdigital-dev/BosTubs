85c43f258363e8d49693adcd349ec841276ce882
BYB Digital <buildyourbranddigital@gmail.com>
2025-09-13 08:15:40 +0000

    as

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+// ✅ Import your global CSS entry here
+import "./index.css"; // or whatever your global file is called
+
+createRoot(document.getElementById("root")!).render(<App />);
